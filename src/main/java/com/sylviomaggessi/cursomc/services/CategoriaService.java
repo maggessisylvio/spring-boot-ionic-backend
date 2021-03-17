9710c861c6c025b6f85930cea599d44908a39571
Sylvio Maggessi <sylviodev@gmail.com>
2021-03-17 02:24:39 -0300

    ajustes no endpoint /categorias/id

--- src/main/java/com/sylviomaggessi/cursomc/services/CategoriaService.java
+++ src/main/java/com/sylviomaggessi/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.sylviomaggessi.cursomc.domain.Categoria;
 import com.sylviomaggessi.cursomc.repositories.CategoriaRepository;
+import com.sylviomaggessi.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
-	private CategoriaRepository repo; 
-	
+	private CategoriaRepository repo;
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}
-	
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
 }
