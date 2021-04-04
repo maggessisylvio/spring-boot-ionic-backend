72ba2ece3e766656b27a2fed93f3e3899bd4f104
Sylvio Maggessi <sylviodev@gmail.com>
2021-04-04 20:29:19 -0300

    Endpoint /pedidos/{id}

--- src/main/java/com/sylviomaggessi/cursomc/domain/ItemPedido.java
+++ src/main/java/com/sylviomaggessi/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 
@@ -28,6 +31,7 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
@@ -96,4 +100,6 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 }
