6baf790e8165642da260966a709046a555135da4
Sylvio Maggessi <sylviodev@gmail.com>
2021-04-13 23:25:46 -0300

    validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/sylviomaggessi/cursomc/domain/enums/TipoCliente.java
+++ src/main/java/com/sylviomaggessi/cursomc/domain/enums/TipoCliente.java
@@ -3,7 +3,7 @@ package com.sylviomaggessi.cursomc.domain.enums;
 public enum TipoCliente {
 	
 	PESSOAFISICA(1, "Pessoa Física"),
-	PESSOAJURIFICA(2, "Pessoa Jurídica");
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
 	
 	private int cod;
 	private String descricao;
