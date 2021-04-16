1848b69f15028f66dbed6920a80b9d50b29d352d
Sylvio Maggessi <sylviodev@gmail.com>
2021-04-15 22:18:01 -0300

    inserindo Pedido

---
+++ src/main/java/com/sylviomaggessi/cursomc/services/BoletoService.java
@@ -0,0 +1,20 @@
+package com.sylviomaggessi.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.sylviomaggessi.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+	
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+
+}
