3935d52af6a8213958e2e701738b182540a9d3f3
Sylvio Maggessi <sylviodev@gmail.com>
2021-04-16 21:49:04 -0300

    email HTML

--- src/main/java/com/sylviomaggessi/cursomc/services/MockEmailService.java
+++ src/main/java/com/sylviomaggessi/cursomc/services/MockEmailService.java
@@ -2,6 +2,8 @@ package com.sylviomaggessi.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,5 +17,14 @@ public class MockEmailService extends AbstractEmailService {
 		LOG.info(msg.toString());
 		LOG.info("Email enviado!");		
 	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado!");
+		
+	}
+	
 
 }
