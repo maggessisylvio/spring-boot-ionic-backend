9034b6e012d48c524bc65dd87a1c4f9114fd030c
Sylvio Maggessi <sylviodev@gmail.com>
2021-04-16 21:19:24 -0300

    implementando SmtpEmailService com servidor do Google

--- src/main/java/com/sylviomaggessi/cursomc/services/DBService.java
+++ src/main/java/com/sylviomaggessi/cursomc/services/DBService.java
@@ -118,7 +118,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "23223223222", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "sylviodev@gmail.com", "23223223222", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("999540679", "991913144"));
 
