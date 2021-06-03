6f382ff54fe5664be15dd6fa8e60af8665574241
Sylvio Maggessi <sylviodev@gmail.com>
2021-06-03 12:22:07 -0300

    salvando imagem no s3 bucket

--- src/main/java/com/sylviomaggessi/cursomc/CursomcApplication.java
+++ src/main/java/com/sylviomaggessi/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.sylviomaggessi.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sylviomaggessi.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -13,6 +19,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\Sylvio Maggessi\\Pictures\\Saved Pictures\\allmight.jpg");
 	}
 
 }
